3219650608cf6f01292152f211a5cb4a38b8513a
zz <zzvr@outlook.com>
2025-04-07 18:35:56 +0800

    修复报错

--- src/ResxResourceExtension/Model/ProjectModel.cs
+++ src/ResxResourceExtension/Model/ProjectModel.cs
@@ -1,9 +1,11 @@
 ﻿namespace ResxResourceExtension.Model
 {
-    internal class ProjectModel(string name, string[] resourceFiles)
+    internal class ProjectModel(string name, string[] resourceFiles) : IComparable
     {
         public string Name { get; } = name;
 
         public string[] ResourceFiles { get; } = resourceFiles;
+
+        public int CompareTo(object obj) => Name.CompareTo(((ProjectModel)obj).Name);
     }
 }
